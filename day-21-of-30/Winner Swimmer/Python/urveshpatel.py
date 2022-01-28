1b834ffaefd92230b0e001dd427140486a58479d
urvesh254 <urveshpatel2001@gmail.com>
2022-01-28 10:46:42 +0530

    Fix: Winner Swimmer issue fixed.

--- day-21-of-30/Winner Swimmer/Python/urveshpatel.py
+++ day-21-of-30/Winner Swimmer/Python/urveshpatel.py
@@ -1,7 +1,8 @@
 n = int(input())
 scores = list(map(int, input().split()))
 
-maxScore = max(scores) + 1
+maxScore = max(scores)
+maxScore += scores.count(maxScore)
 count = 0
 for score in scores:
     if maxScore - score <= n:
